95629a64d04f63ad339caa92dfb8f562f88d91ae
Sandy Lumacad <officialsandy.ss@gmail.com>
2025-12-11 00:45:58 +0800

    (styles): Input fields in ProfilePage

--- client/src/components/ProfilePage.js
+++ client/src/components/ProfilePage.js
@@ -85,7 +85,7 @@ function Button({
 function Input(props) {
   return (
     <input
-      className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm"
+      className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm profile-input"
       {...props}
     />
   );
